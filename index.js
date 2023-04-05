7f3b0d927365248124f5a54f2171c604715a0d86
TabasumNad <tmnadaf17@gmail.com>
2023-04-05 23:34:34 +0530

    mobile api

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import usersRouter from "./router/users.router.js"
 
 
 
+
 const app = express();
 
 // const PORT = 4000;
@@ -42,6 +43,54 @@ app.get("/", function (request, response) {
 app.use('/movies',moviesRouter);
 app.use('/users',usersRouter);
 // app.use('/login',loginRouter);
+
+// DemoApp for FSD day 1--->creating API for mobile
+// app.use('/mobile',mobileRouter);
+const mobiles=[
+  {
+    "model": "OnePlus 9 5G",
+    "img": "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
+    "company": "Oneplus"
+  },
+  {
+    "model": "Iphone 13 mini",
+    "img": "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
+    "company": "Apple"
+  },
+  {
+    "model": "Samsung s21 ultra",
+    "img": "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
+    "company": "Samsung"
+  },
+  {
+    "model": "Xiomi mi 11",
+    "img": "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
+    "company": "Xiomi"
+  }
+]
+
+app.get("/mobile", async function (request, response) {
+
+  const result= await client.
+  db("backend_connection")
+  .collection("mobiles")
+  .find({})
+  .toArray();
+  
+  response.send(mobiles);
+});
+
+app.post("/mobile", async function (request, response) {
+
+  const data=request.body;
+    console.log(data);
+
+    const result= await client.
+    db("backend_connection")
+    .collection("mobiles")
+    .insertMany(data);
+  response.send(result);
+});
 
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
--- service/movies.service.js
+++ service/movies.service.js
@@ -1,14 +1,17 @@
 import { client } from "../index.js";
+import { ObjectId } from "mongodb";
 
 export async function updateMovieById(id, data) {
   return await client.db("backend_connection")
     .collection("mockmovie")
     .updateOne({ id: id }, { $set: data });
+    // .updateOne({ _id: new ObjectId(id) }, { $set: data });
 }
 export async function deleteMovieById(id) {
   return await client.db("backend_connection")
     .collection("mockmovie")
-    .deleteOne({ id: id });
+    .deleteOne({ id: id});
+    // .deleteOne({ _id: new ObjectId(id)});
 }
 export async function createMovie(data) {
   return await client.db("backend_connection")
@@ -18,7 +21,8 @@ export async function createMovie(data) {
 export async function getMovieById(id) {
   return await client.db("backend_connection")
     .collection("mockmovie")
-    .findOne({ id: id });
+    .findOne({ id: id});
+    // .findOne({ _id: new ObjectId(id) });
 }
 export async function getAllMovies(query) {
   if(query.rating){
